edd2570ca26d89949d0773a546ea09ab53ba9169
soul <soularis109@gmail.com>
2021-12-30 13:07:06 +0200

    new calc

---
+++ assets/js/navigation.js
@@ -0,0 +1,115 @@
+let navigationItem = document.querySelectorAll('.navigation__block');
+
+
+const link__min = document.getElementById("btn__minimal")
+const link__stan = document.getElementById("btn__standart")
+const link__vip = document.getElementById("btn__vip")
+
+    navigationItem.forEach((item,i) => {
+    item.addEventListener('click', function () {
+
+        if(item.classList.contains('navigation__block-month')){
+            //////////minimal///////////////////
+            document.querySelector('.subscription__price--minimal').textContent = '5 $';
+            document.querySelector('.blue__minimal').textContent = '';
+            document.querySelector('.total__price--minimal').textContent = '205 $';
+            //////////standart///////////////////
+            document.querySelector('.subscription__price--standart').textContent = '10 $';
+            document.querySelector('.blue__standart').textContent = '';
+            document.querySelector('.total__price--standart').textContent = '260 $';
+            //////////vip///////////////////
+            document.querySelector('.subscription__price--vip').textContent = '20 $';
+            document.querySelector('.blue__vip').textContent = '';
+            document.querySelector('.total__price--vip').textContent = '270 $';
+            link__min.href = 'https://secure.wayforpay.com/button/b0219fcb62865'
+            link__stan.href = 'https://secure.wayforpay.com/button/b9162d6536840'
+            link__vip.href = 'https://secure.wayforpay.com/button/bcc85742109ab'
+
+
+        }
+        else if(item.classList.contains('navigation__block-month-select')) {
+            //////////minimal by select///////////////////
+            document.querySelector('.subscription__price--minimal').textContent = '5 $';
+            document.querySelector('.blue__minimal').textContent = '';
+            document.querySelector('.total__price--minimal').textContent = '205 $';
+            link__min.href = 'https://secure.wayforpay.com/button/b0219fcb62865'
+            link__stan.href = 'https://secure.wayforpay.com/button/b9162d6536840'
+            link__vip.href = 'https://secure.wayforpay.com/button/bcc85742109ab'
+        }
+        else if(item.classList.contains('navigation__block-years')){
+            //////////minimal///////////////////
+            document.querySelector('.subscription__price--minimal').textContent = '4.25 $';
+            document.querySelector('.blue__minimal').textContent = '5 $';
+            document.querySelector('.total__price--minimal').textContent = '251 $';
+            //////////standart///////////////////
+            document.querySelector('.subscription__price--standart').textContent = '8.50 $';
+            document.querySelector('.blue__standart').textContent = '10 $';
+            document.querySelector('.total__price--standart').textContent = '352 $';
+            //////////vip///////////////////
+            document.querySelector('.subscription__price--vip').textContent = '17 $';
+            document.querySelector('.blue__vip').textContent = '20 $';
+            document.querySelector('.total__price--vip').textContent = '454 $';
+            link__min.href = 'https://secure.wayforpay.com/button/be876b918d414'
+            link__stan.href = 'https://secure.wayforpay.com/button/ba95b7bfe5e7b'
+            link__vip.href = 'https://secure.wayforpay.com/button/badab653b96a9'
+
+
+
+        }
+        else if(item.classList.contains('navigation__block-two-years')) {
+            //////////minimal///////////////////
+            document.querySelector('.subscription__price--minimal').textContent = '3.75 $';
+            document.querySelector('.blue__minimal').textContent = '5 $';
+            document.querySelector('.total__price--minimal').textContent = '290 $';
+            //////////standart///////////////////
+            document.querySelector('.subscription__price--standart').textContent = '7.50 $';
+            document.querySelector('.blue__standart').textContent = '10 $';
+            document.querySelector('.total__price--standart').textContent = '430 $';
+            //////////vip///////////////////
+            document.querySelector('.subscription__price--vip').textContent = '15 $';
+            document.querySelector('.blue__vip').textContent = '20 $';
+            document.querySelector('.total__price--vip').textContent = '610 $';
+            link__min.href = 'https://secure.wayforpay.com/button/bf0027f51d0a8'
+            link__stan.href = 'https://secure.wayforpay.com/button/be166d92f73d0'
+            link__vip.href = 'https://secure.wayforpay.com/button/b0244b4100877'
+
+        }
+        else if(item.classList.contains('navigation__block-three-years')) {
+            //////////minimal///////////////////
+            document.querySelector('.subscription__price--minimal').textContent = '3.25 $';
+            document.querySelector('.blue__minimal').textContent = '5 $';
+            document.querySelector('.total__price--minimal').textContent = '317 $';
+            //////////standart///////////////////
+            document.querySelector('.subscription__price--standart').textContent = '6.50 $';
+            document.querySelector('.blue__standart').textContent = '10 $';
+            document.querySelector('.total__price--standart').textContent = '484 $';
+            //////////vip///////////////////
+            document.querySelector('.subscription__price--vip').textContent = '13 $';
+            document.querySelector('.blue__vip').textContent = '20 $';
+            document.querySelector('.total__price--vip').textContent = '718 $';
+            link__min.href = 'https://secure.wayforpay.com/button/b619b74428f7c'
+            link__stan.href = 'https://secure.wayforpay.com/button/b06618abd62de'
+            link__vip.href = 'https://secure.wayforpay.com/button/bdc9ef2d67194'
+
+        }
+        else if(item.classList.contains('navigation__block-four-years')) {
+            //////////minimal///////////////////
+            document.querySelector('.subscription__price--minimal').textContent = '2.50 $';
+            document.querySelector('.blue__minimal').textContent = '5 $';
+            document.querySelector('.total__price--minimal').textContent = '320 $';
+            //////////standart///////////////////
+            document.querySelector('.subscription__price--standart').textContent = '5 $';
+            document.querySelector('.blue__standart').textContent = '10 $';
+            document.querySelector('.total__price--standart').textContent = '490 $';
+            //////////vip///////////////////
+            document.querySelector('.subscription__price--vip').textContent = '10 $';
+            document.querySelector('.blue__vip').textContent = '20 $';
+            document.querySelector('.total__price--vip').textContent = '730 $';
+            link__min.href = 'https://secure.wayforpay.com/button/bd1b2c989587e'
+            link__stan.href = 'https://secure.wayforpay.com/button/bf90ceeb48aef'
+            link__vip.href = 'https://secure.wayforpay.com/button/beb1fee5cde0f'
+        }
+
+
+    })
+})
---
+++ assets/js/select.js
@@ -0,0 +1,53 @@
+let select = function () {
+        let selectHeader = document.querySelectorAll
+        ('.navigation__header')
+    let selectItem = document.querySelectorAll('.navigation');
+    let selectBlock = document.querySelectorAll('.navigation__body .navigation');
+    let row = document.querySelector('.row_nav')
+    selectHeader.forEach(item => {
+        item.addEventListener('click', function (){
+
+            this.parentElement.classList.toggle('is-active')
+        })})
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)})
+
+
+
+    const actives = document.getElementsByClassName('active');
+    for ( let i = 0; selectBlock.length > i; i++) {
+        selectBlock[i].onclick = function() {
+            const currentActive = actives[1];
+            if (currentActive)
+                currentActive.classList.remove("active");
+
+            if (currentActive !== this)
+                this.classList.add("active");
+        };
+    }
+    // for (var i = 0; i < selectBlock.length; i++) {
+    //     selectBlock[i].addEventListener("click", function() {
+    //         var current = document.getElementsByClassName("active");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         this.className += " active";
+    //     });
+    // }
+
+    function selectChoose() {
+        let text = this.innerHTML;
+        let currentText = this.closest('.row_nav')
+        .querySelector('.navigation__select')
+        currentText.focus();
+        currentText.innerHTML = text;
+        row.classList.remove('is-active')
+
+
+
+        }
+
+
+
+
+}
+select();
